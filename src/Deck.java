483dd91fb37a4e463592ade516824a6908a59bd1
Nazeer Holmes <ns_holmes@nazeerholmes.com>
2012-06-23 12:53:43 -0500

    included comments

--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,5 @@
 public class Deck {
 	public Card c;
-	public char[] suitArr =  {'h','d','s','c'};
+	public char[] suitArr =  {'h','d','s','c'};//h-hearts, d= diamonds, s=spades, c=clubs
 	public String[] kindArr = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 }
